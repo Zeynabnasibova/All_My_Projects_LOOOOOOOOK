dc6e850e21c436eb1fe43c31e6153b7fa9deacee
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-01 15:27:53 -0400

    EndsLy

---
+++ src/codingBat/string_1/EndsLy.java
@@ -0,0 +1,31 @@
+package codingBat.string_1;
+
+public class EndsLy {
+    /**
+     *
+     Given a string, return true if it ends in "ly".
+
+
+     endsLy("oddly") → true
+     endsLy("y") → false
+     endsLy("oddy") → false
+     */
+
+    public static void main(String[] args) {
+        System.out.println(endsLy("oddly"));
+    }
+
+    public static boolean endsLy(String str) {
+        //return str.endsWith("ly");
+
+
+        if(str.endsWith("ly") ){
+
+            return true;
+        } else {
+
+            return false;
+
+        }
+    }
+}
