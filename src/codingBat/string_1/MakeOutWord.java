4ab6acc4eee958b49d048096acf24b32b0057710
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-01 15:14:00 -0400

    MakeOutWord

--- src/codingBat/string_1/MakeOutWord.java
+++ src/codingBat/string_1/MakeOutWord.java
@@ -4,7 +4,7 @@ public class MakeOutWord {
 
     /**
      *
-     Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+     Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".. Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
 
 
      makeOutWord("<<>>", "Yay") → "<<Yay>>"
