61f40c6f7bbc30ca1af02ccf0f08135a21c4e72d
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-01 15:01:31 -0400

    HelloName

---
+++ src/codingBat/string_1/HelloName.java
@@ -0,0 +1,31 @@
+package codingBat.string_1;
+
+public class HelloName {
+
+    /**
+     * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+     *
+     *
+     * helloName("Bob") → "Hello Bob!"
+     * helloName("Alice") → "Hello Alice!"
+     * helloName("X") → "Hello X!"
+     *
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println(helloName("Bob"));
+
+    }
+
+
+    public static String helloName(String name) {
+
+        name = "Hello ".concat(name) + "!" ;
+
+        return name;
+
+
+    }
+
+}
