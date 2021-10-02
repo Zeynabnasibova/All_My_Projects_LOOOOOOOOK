4cae558b41aa1bc5bc8bb1eb241e3cb00dd34dc4
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 00:52:01 -0400

    Ex60

---
+++ src/w3resource_com/BasicPart_1/Ex60.java
@@ -0,0 +1,28 @@
+package w3resource_com.BasicPart_1;
+
+public class Ex60 {
+
+    public static void main(String[] args) {
+
+
+        /*
+
+        Write a Java program to find the penultimate (next to last) word of a sentence. Go to the editor
+Sample Output:
+
+Input a String: The quick brown fox jumps over the lazy dog.
+Penultimate word: lazy
+         */
+
+        String sentence = "The quick brown fox jumps over the lazy dog.";
+
+        String [] arrSentence = sentence.split(" ");
+
+        String newSentence = arrSentence[arrSentence.length - 2];
+
+        System.out.println(newSentence);
+
+
+
+    }
+}
