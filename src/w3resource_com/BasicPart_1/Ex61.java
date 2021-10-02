51a94b4ec170c1d297134a90bb5681c9f9a5bddf
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 00:52:17 -0400

    Ex61

---
+++ src/w3resource_com/BasicPart_1/Ex61.java
@@ -0,0 +1,23 @@
+package w3resource_com.BasicPart_1;
+
+public class Ex61 {
+
+    public static void main(String[] args) {
+
+
+        /*
+Write a Java program to reverse a word. Go to the editor
+Sample Output:
+
+Input a word: dsaf
+Reverse word: fasd
+
+
+         */
+
+String sentence = "dsaf";
+
+
+
+    }
+}
