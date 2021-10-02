03fbc70469a618365b69b990132774ed87776468
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 00:52:37 -0400

    Practice

---
+++ src/w3resource_com/BasicPart_1/Practice.java
@@ -0,0 +1,23 @@
+package w3resource_com.BasicPart_1;
+
+import java.util.Arrays;
+
+public class Practice {
+
+
+    public static void main(String[] args) {
+
+        String sentence = "the quick brown fox jumps over the lazy dog.";
+
+        String [] newStr = sentence.split(" ");
+
+        System.out.println(Arrays.toString(newStr));
+
+        System.out.println();
+
+        char[] arr1 = sentence.toCharArray();
+
+        System.out.println(arr1);
+
+    }
+}
