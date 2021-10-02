a4bd75b0f7088bc2e1de15bd70826b7233e056f7
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 00:49:11 -0400

    Q2

---
+++ src/java_Interview_CodingTask_Implementation_1_B24/Q2.java
@@ -0,0 +1,34 @@
+package java_Interview_CodingTask_Implementation_1_B24;
+
+import java.util.Arrays;
+
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(SameLetters());
+    }
+        /*
+Q2: String -- Same letters
+Write a return method that check if a string is build out of the same letters as another string.
+Ex: same("abc", "cab"); -> true
+same("abc", "abb"); -> false:
+
+         */
+public static boolean SameLetters(){
+
+        String str1 = "abc";
+        String str2 = "cab";
+
+        char[] arr1 = str1.toCharArray();  //  first I convert the strings into char[] arrays
+
+        char[] a2 = str2.toCharArray();
+
+        Arrays.sort(arr1); // Then I sort this array
+        Arrays.sort(a2);
+
+        return Arrays.equals(arr1, a2); // check for equality
+
+
+    }
+}
