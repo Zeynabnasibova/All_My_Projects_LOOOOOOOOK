43f434a97dd99f6dec242860372dfb5181ccd44b
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 00:51:46 -0400

    Ex59

---
+++ src/w3resource_com/BasicPart_1/Ex59.java
@@ -0,0 +1,25 @@
+package w3resource_com.BasicPart_1;
+
+import java.util.Locale;
+
+public class Ex59 {
+
+    public static void main(String[] args) {
+
+/*
+59. Write a Java program to convert a given string into lowercase. Go to the editor
+Sample Output:
+
+Input a String: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+the quick brown fox jumps over the lazy dog.
+
+ */
+
+        String sentence = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+
+        String newSentence = sentence.toLowerCase();
+
+        System.out.println(newSentence);
+
+    }
+}
