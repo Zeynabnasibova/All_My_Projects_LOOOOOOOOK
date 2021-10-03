44d67e8b4e413ac719b2f3d197495d809a4025d8
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 21:45:18 -0400

    Q4

---
+++ src/codesDope/practiceJava/knowData_types/Q4.java
@@ -0,0 +1,19 @@
+package codesDope.practiceJava.knowData_types;
+
+public class Q4 {
+
+    public static void main(String[] args) {
+
+        /*
+        Print the ASCII value of the character 'h'.
+         */
+
+
+        char ch = 'h';
+
+        int ascii = (int)ch;
+
+        System.out.println("ASCII value of the character \" h \" is: " + ascii);
+
+    }
+}
