653a9f9b0cb0934b3289c8268b383cac690cb03e
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-03 00:52:50 -0400

    Q16

---
+++ src/codesDope/practiceJava/javaOperators/Q16.java
@@ -0,0 +1,41 @@
+package codesDope.practiceJava.javaOperators;
+
+public class Q16 {
+
+    public static void main(String[] args) {
+
+
+        /*
+        Write a program to reverse a 3-digit number. E.g.-Number : 132        Output : 231
+         */
+
+
+        //int num = 123;
+
+        int reverse = 0;
+
+        for(int num = 123;num != 0; num /= 10){
+
+            reverse = (reverse * 10) + num % 10;
+
+
+        }
+
+        System.out.println(reverse);
+
+        System.out.println();
+
+        int rev = 0;
+        int number = 123;
+
+        while(number != 0){
+
+            int digit = number% 10;
+
+            number /= 10;
+
+            rev = rev * 10 + digit;
+        }
+        System.out.println(rev);
+    }
+}
