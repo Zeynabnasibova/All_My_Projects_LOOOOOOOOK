737f4bb6d80141ba8575836d5161483d13c0b909
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 21:46:01 -0400

    Q7

---
+++ src/codesDope/practiceJava/knowData_types/Q7.java
@@ -0,0 +1,21 @@
+package codesDope.practiceJava.knowData_types;
+
+public class Q7 {
+
+    public static void main(String[] args) {
+
+
+        /*
+Write a program to add an integer variable having value 5 and a double variable having value 6.2.
+         */
+
+        int num1 = 5;
+        double num2 = 6.2;
+
+        double add = num1 + num2;
+
+        System.out.println(add);
+
+
+    }
+}
