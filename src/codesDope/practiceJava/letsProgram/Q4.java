f558a44ef2a4d8e6879744453899c8aeb25f3934
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-02 21:47:13 -0400

    Q4

---
+++ src/codesDope/practiceJava/letsProgram/Q4.java
@@ -0,0 +1,16 @@
+package codesDope.practiceJava.letsProgram;
+
+public class Q4 {
+
+    public static void main(String[] args) {
+
+        /*
+        Write a program to print the sum of the numbers 2, 4 and 5.
+         */
+
+        System.out.println(2+4+5);
+
+
+
+    }
+}
