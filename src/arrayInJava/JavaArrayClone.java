cf44263f927cb1ceaf03eeb42a170b395b5d51dc
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-04 11:09:45 -0400

    Array Clone

---
+++ src/arrayInJava/JavaArrayClone.java
@@ -0,0 +1,18 @@
+package arrayInJava;
+
+public class JavaArrayClone {
+
+    public static <Employee> void main(String[] args) {
+
+        /**
+         * 1. Array Clone – Shallow Copy
+         *
+         * In Java, to create clone of array, you should use clone() method of array. It creates a shallow copy of array.
+         *
+         * Cloning always creates shallow copy of array. Any change (in original array) will be reflected in cloned array as well.
+         */
+
+
+
+    }
+}
