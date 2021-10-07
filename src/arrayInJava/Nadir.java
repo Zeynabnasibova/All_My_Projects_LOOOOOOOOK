c18b884f4c1ef4fa82ba4cae72fafe072b79b374
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-07 08:17:48 -0400

    Last three

---
+++ src/arrayInJava/Nadir.java
@@ -0,0 +1,24 @@
+package arrayInJava;
+
+public class Nadir {
+
+    public static void main(String[] args) {
+
+        System.out.println(lastThree("SoftSkill"));
+
+    }
+
+    public static  boolean lastThree(String str){
+
+        String lastThree = str.substring(str.length()-3);
+
+            if(lastThree.equals("ill")){
+
+                return true;
+
+            } else {
+
+                return false;
+        }
+    }
+}
