93d7fed55da7c5ebfea6d1de18177522a4a4b1f0
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-07 08:16:23 -0400

    Subarray to Arraylist

---
+++ src/arrayList/SubarraytoArraylist.java
@@ -0,0 +1,26 @@
+package arrayList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SubarraytoArraylist {
+
+    public static void main(String[] args) {
+
+/**
+ 2. Subarray to Arraylist
+
+ Java example to create arraylist from subarray. It is done in two steps:
+
+ Create subarray from array with desired items.
+ Convert array to list.
+ */
+        String[] names = {"Alex", "Brian", "Charles", "David"};
+
+//Array to sublist
+        List<String> namesList = Arrays.asList( Arrays.copyOfRange(names, 0, 2) );
+
+
+        System.out.println(namesList);
+    }
+}
