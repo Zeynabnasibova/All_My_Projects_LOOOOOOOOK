3a85ef1b2cd4e2f1026adcd095b640668f42af4b
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-08 00:31:25 -0400

    Java – Print array elements

--- src/arrayInJava/PrintArrayElements.java
+++ src/arrayInJava/PrintArrayElements.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class JavaPrintArrayElements {
+public class PrintArrayElements {
 
     public static void main(String[] args) {
 
