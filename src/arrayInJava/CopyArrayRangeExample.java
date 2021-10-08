ebb79dcbe8d9b388b021a9e36e7dda369d5d187d
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-07 22:48:42 -0400

    coppy array range

--- src/arrayInJava/CopyArrayRangeExample.java
+++ src/arrayInJava/CopyArrayRangeExample.java
@@ -48,17 +48,36 @@ public class CopyArrayRangeExample {
         Copy array range examples
 
         */
+
+        /**
+         Java example to create subarray from array i.e. creating array slice. Learn to use Java 8 Arrays.copyOfRange() method along with converting the subarray to list object.
+         */
+
+
         String[] names = {"Alex", "Brian", "Charles", "David"};         // [Alex, Brian]
 
-        //Copy till second name from with index '0'
-        String[] partialNames = Arrays.copyOfRange(names, 0, 2);        // [Charles, David]
+        System.out.println(Arrays.toString(names));
+        //[Alex, Brian, Charles, David]
 
-        //Copy all names from with index '2'
-        String[] endNames = Arrays.copyOfRange(names, 2, names.length); // [Charles, David, null, null, null, null, null, null]
+//Copy till second name from with index '0'
+        String[] partialNames = Arrays.copyOfRange(names, 0, 2);
 
-        //Copy last 8 names start with index '2'
-        //No ArrayIndexOutOfBoundsException error
+        System.out.println(Arrays.toString(partialNames));
+//[Alex, Brian]
+
+//Copy all names from with index '2'
+        String[] endNames = Arrays.copyOfRange(names, 2, names.length);
+
+        System.out.println(Arrays.toString(endNames));
+        //[Charles, David]
+
+//Copy last 8 names start with index '2'
+//No ArrayIndexOutOfBoundsException error
         String[] moreNames = Arrays.copyOfRange(names, 2, 10);
+
+        System.out.println(Arrays.toString(moreNames));
+        //[Charles, David, null, null, null, null, null, null]
+
 
         /**
         2. Subarray to Arraylist
