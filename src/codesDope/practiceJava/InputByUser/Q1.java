6bf310e24832f08858fea83c91767e7fe019f334
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-13 13:50:58 -0400

    print sum and product of them.

---
+++ src/codesDope/practiceJava/InputByUser/Q1.java
@@ -0,0 +1,25 @@
+package codesDope.practiceJava.InputByUser;
+import java.util.Scanner;
+public class Q1 {
+
+    public static void main(String[] args) {
+
+
+
+        /*
+        Write a program to take two integer inputs from user and print sum and product of them.
+         */
+
+        Scanner input = new Scanner (System.in);
+
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        int product = num1 * num2;
+
+        System.out.println(sum);
+        System.out.println(product);
+
+    }
+}
