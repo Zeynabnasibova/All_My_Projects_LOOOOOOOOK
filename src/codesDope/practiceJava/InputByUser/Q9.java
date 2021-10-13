8b7b6cde248da4b7185f7dde5ec475758bc23fe9
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-13 15:06:04 -0400

    .

---
+++ src/codesDope/practiceJava/InputByUser/Q9.java
@@ -0,0 +1,13 @@
+package codesDope.practiceJava.InputByUser;
+
+public class Q9 {
+
+    public static void main(String[] args) {
+
+        /*
+        Write a program to enter the values of two variables 'a' and 'b' from keyboard and then check if both the conditions 'a < 50' and 'a < b' are true.
+         */
+
+
+    }
+}
