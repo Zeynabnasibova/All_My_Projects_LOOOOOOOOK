a48cb3793d0b4323794b6f0c2faa941ff47739a5
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-13 17:56:12 -0400

    .

---
+++ src/codesDope/practiceJava/decideIfOrElse/Q10.java
@@ -0,0 +1,34 @@
+package codesDope.practiceJava.decideIfOrElse;
+
+public class Q10 {
+
+
+    public static void main(String[] args) {
+        /*
+        If
+x = 2
+y = 5
+z = 0
+then find values of the following expressions:
+a. x == 2
+b. x != 5
+c. x != 5 && y >= 5
+d. z != 0 || x == 2
+e. !(y < 10)
+         */
+
+
+        int x = 2;
+        int y = 5;
+        int z = 0;
+
+        System.out.println(x == 2);
+        System.out.println(x != 5);
+        System.out.println(x != 0);
+        System.out.println( x != 5 && y >= 5);
+        System.out.println(z != 0 || x == 2);
+        System.out.println(!(y < 10));
+
+
+    }
+}
