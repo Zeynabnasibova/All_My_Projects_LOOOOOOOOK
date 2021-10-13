b2f7a2056fb1adf3120b7d162f1e57830e5dffd6
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-13 16:48:13 -0400

    .

---
+++ src/codesDope/practiceJava/decideIfOrElse/Q8.java
@@ -0,0 +1,21 @@
+package codesDope.practiceJava.decideIfOrElse;
+
+public class Q8 {
+
+    public static void main(String[] args) {
+
+        /*
+        A student will not be allowed to sit in exam if his/her attendence is less than 75%.
+Take following input from user
+Number of classes held
+Number of classes attended.
+And print
+percentage of class attended
+Is student is allowed to sit in exam or not.
+         */
+
+
+
+
+    }
+}
