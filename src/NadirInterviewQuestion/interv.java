5cb9277b2628c6e9a62e16da20beb69615176def
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-13 13:51:10 -0400

    .

--- src/NadirInterviewQuestion/interv.java
+++ src/NadirInterviewQuestion/interv.java
@@ -9,10 +9,13 @@ create metod which will acsept two string return str
 
     public static void main(String[] args) {
 
+
         System.out.println(contains("Nadir","Nad"));
     }
 
     public static boolean contains(String str1,String str2){
+
+
 
         if(str1.contains(str2)){
 
--- src/codesDope/practiceJava/javaOperators/Q14_EachNumberwithwhileloop.java
+++ src/codesDope/practiceJava/javaOperators/Q14_EachNumberwithwhileloop.java
@@ -14,7 +14,7 @@ public class Q14_EachNumberwithwhileloop {
 
         while(number > 0 && y > 0) {
 
-       eachnum = number / y;//5
+       eachnum = number / y;//5  5  56 569 1569
 
             int x = y /10;
 
