a2830ff80b976831b483832832d6f499fef2a4a8
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-13 16:34:46 -0400

    .

--- src/codesDope/practiceJava/decideIfOrElse/Q3.java
+++ src/codesDope/practiceJava/decideIfOrElse/Q3.java
@@ -11,33 +11,6 @@ Suppose, one unit will cost 100.
 Judge and print total cost for user.
          */
 
-   Scanner input = new Scanner(System.in);
-   System.out.println("Enter marks");
-   int marks = input.nextInt();
-
-   if(marks < 25){
-
-       System.out.println("F");
-
-   }else if(marks >= 25 && marks < 45){
-
-       System.out.println("E");
-
-   }else if(marks >= 45 && marks < 50){
-
-       System.out.println("D");
-   } else if(marks >= 50 && marks < 60 ) {
-
-       System.out.println("C");
-   }else if(marks >= 60 && marks < 80 ){
-
-       System.out.println("B");
-   }else if(marks >= 80){
-
-       System.out.println("A");
-   }
-
-
 
 
 
--- src/codesDope/practiceJava/decideIfOrElse/Q5.java
+++ src/codesDope/practiceJava/decideIfOrElse/Q5.java
@@ -1,5 +1,7 @@
 package codesDope.practiceJava.decideIfOrElse;
 
+import java.util.Scanner;
+
 public class Q5 {
     public static void main(String[] args) {
 
@@ -14,5 +16,36 @@ e. 60 to 80 - B
 f. Above 80 - A
 Ask user to enter marks and print the corresponding grade.
          */
+
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter marks");
+        int marks = input.nextInt();
+
+        if(marks < 25){
+
+            System.out.println("F");
+
+        }else if(marks >= 25 && marks < 45){
+
+            System.out.println("E");
+
+        }else if(marks >= 45 && marks < 50){
+
+            System.out.println("D");
+        } else if(marks >= 50 && marks < 60 ) {
+
+            System.out.println("C");
+        }else if(marks >= 60 && marks < 80 ){
+
+            System.out.println("B");
+        }else if(marks >= 80){
+
+            System.out.println("A");
+        }
+
+
+
     }
 }
