dc1aa11f682e7fd149c37efbcde4f2280f23fed2
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-13 12:23:54 -0400

    .

---
+++ src/leetcode/LongestCommonSubsequence.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class LongestCommonSubsequence {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/leetcode/ReverstInteger.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class ReverstInteger {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(reverseInt(12345));
+    }
+
+    public static int reverseInt(int x) {
+
+
+        int reverse = 0;
+
+        for (; x != 0; x /= 10) {
+
+            reverse = (reverse * 10) + x % 10;
+
+        }
+return reverse;
+    }
+}
