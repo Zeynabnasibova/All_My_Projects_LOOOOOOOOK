9ea859089c442cf35875d091d8c9229dd58ecf1c
Zeynab <zeynabnasibova7@gmail.com>
2021-10-25 11:42:55 -0400

    .

--- src/replit/statements2/TernaryOperator1.java
+++ src/replit/statements2/TernaryOperator1.java
@@ -1,16 +1,24 @@
 package replit.statements2;
 
+import java.util.Scanner;
+
 public class TernaryOperator1 {
 
-    /*
-    Write an expression using the conditional operator (? :) that compares the values of the variables num1 and num2. The result (that is the value) of this expression should be the value of the larger of the two variables. The larger value should be printed out.
-     */
+   /*
+   Write an expression using the conditional operator (? :) that compares the value of the variable x to 5 and results in:
+
+Display x if x is greater than or equal to 5 Display -x if x is less than 5
+
+DO NOT USE IF STATEMENT or SWITCH CASE
+    */
 
     public static void main(String[] args) {
 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter number:");
+    int x = scan.nextInt();
 
-
-
+        System.out.println((x >= 5) ? x : -x);
 
 
     }
