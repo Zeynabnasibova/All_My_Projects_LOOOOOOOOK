a31ef40a80073662ed734b09458350d5c9d04285
Zeynab <zeynabnasibova7@gmail.com>
2021-10-26 21:11:58 -0400

    .

---
+++ src/replit/String3/MiddleThree.java
@@ -0,0 +1,37 @@
+package replit.String3;
+import java.util.Scanner;
+public class MiddleThree {
+
+    public static void main(String []args){
+
+        /*
+        Given a String variable word print the middle three characters if the word is an odd number of characters and has more than 5 characters. If the word does not meet the requirements print invalid.
+
+fifteen ==> fte
+
+apple ==> ppl
+
+hey ==> invalid
+
+java ==> invalid
+
+whatsup ==> ats
+
+$ ==> invalid
+         */
+
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+
+        if(word.length() % 2 == 1 && word.length() >= 5){
+
+            String middleThree = word.substring(word.length()/2 -1,word.length()/2+1) + word.substring(word.length()/2 +1,word.length()/2 +2);
+            System.out.println(middleThree);
+
+        }else{
+
+            System.out.println("Invalid");
+        }
+
+    }
+}
--- src/replit/String3/PrinthalfTwice.java
+++ src/replit/String3/PrinthalfTwice.java
@@ -1,11 +1,30 @@
 package replit.String3;
 
+import java.util.Scanner;
+
 public class PrinthalfTwice {
 
     public static void main(String[] args) {
 
-        String str = "apple";
-        String reverse = str.substring(str.length()-1);
-        System.out.println(reverse);
+        /*
+        Write a program that will print out first half of the word twice.
+
+Example:
+
+input: java
+
+output: jaja
+
+input: unity
+
+output: unun
+
+         */
+
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+        String twice = word.substring(0,word.length()/2);
+        System.out.println(twice.concat(twice));
+
     }
 }
